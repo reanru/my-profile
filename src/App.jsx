acc4a110e27071e5a260370beeb4144cf317903a
reanru <runtufandi@gmail.com>
2025-04-04 21:53:08 +0800

    setup layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { Route, Routes } from "react-router";
+
 import './App.css'
+
+import Layout from "./layout/views/index";
+import MainModule from "./moduls/mainModule/views/index";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Routes>
+        <Route path="/*" element={<Layout></Layout>} />
+      </Routes>
     </>
   )
 }
